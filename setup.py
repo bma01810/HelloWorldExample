21ee4d90fb1f6b1c96f93743857283366508cf56
bma01810 <95444741+bma01810@users.noreply.github.com>
2024-10-17 11:40:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='0.1.0',
     packages=find_packages(include=['exampleproject', 'exampleproject.*']),
     install_requires=[
-        'exp10it'==2.6.70,
-        'gradio'==4.16.0,
+        'exp10it==2.6.70',
+        'gradio==4.16.0',
     ],
 )
