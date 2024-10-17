429424a0da9528f4a0e628ff73a300b35e5540ab
bma01810 <95444741+bma01810@users.noreply.github.com>
2024-10-17 11:30:29 -0400

    adding install requires to setup PY

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name='Hello_World',
     version='0.1.0',
-    packages=find_packages(include=['exampleproject', 'exampleproject.*'])
+    packages=find_packages(include=['exampleproject', 'exampleproject.*']),
+    install_requires=[
+        'exp10it'==2.6.70,
+        'gradio'==4.16.0,
+    ],
 )
